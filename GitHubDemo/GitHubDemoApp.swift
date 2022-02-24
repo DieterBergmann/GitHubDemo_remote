c3a18c0d97f6935b60904d245075840757923844
Dieter Bergmann <dieterbergmann@arcor.de>
2022-02-24 06:00:13 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Dieter Bergmann on 24.02.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
