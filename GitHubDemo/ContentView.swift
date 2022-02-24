8b329ca8d5f13d47dbe3cac8f148585c7b950b35
Dieter Bergmann <dieterbergmann@arcor.de>
2022-02-24 06:09:07 +0100

    Change text content

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      Text("This is a test")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
     }
 }
