c60f2dc016245a345e357c5c8d33f3a6b136dcc2
Dieter Bergmann <dieterbergmann@arcor.de>
2022-02-24 19:57:58 +0100

    Re-Indent

--- GitHubDemo/MainView.swift
+++ GitHubDemo/MainView.swift
@@ -12,13 +12,13 @@ struct MainView: View {
     // MARK: - Body
     
     var body: some View {
-      Text("This is a test")
-        .padding()
+        Text("This is a test")
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-      MainView()
+        MainView()
     }
 }
